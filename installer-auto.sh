688f6f766e09e511022487d90f46619449c0cd6a
unknownubuntu <unknownubuntu@proton.me>
2022-12-03 04:13:55 +0100

    test1

--- installer-auto.sh
+++ installer-auto.sh
@@ -29,10 +29,10 @@ clear -x
 # installasjons script
 echo "[-] Installer Program !"
 
-read -p "vil du installere Standard Opsett? [Y/n] (N for nei) " STD Opsett
+read -p "vil du installere Standard Opsett? [Y/n] (N for nei) " tor
 
 case $tor in
-	[Yy]* ) apt install software-properties-common git gh curl axel wget libnewt-dev libssl-dev libncurses5-dev subversion libsqlite3-dev build-essential libjansson-dev libxml2-dev uuid-dev install python2.7 python3.10-full gpg gufw synaptic snap snapd preload install ubuntu-restricted-extras  -y; break;;
+	[Yy]* ) apt install software-properties-common git gh curl axel wget libnewt-dev libssl-dev libncurses5-dev subversion libsqlite3-dev build-essential libjansson-dev libxml2-dev uuid-dev python2.7 python3.10-full gpg gufw synaptic snap snapd preload ubuntu-restricted-extras openssh-server openssh-client -y && mv /etc/apt/trusted.gpg /etc/apt/trusted.gpg.d/ && sudo ufw allow ssh && gsettings set org.gnome.shell.extensions.dash-to-dock click-action ‘minimize’; break;;
 	[Nn]* ) break;;
        	* ) echo "You didn't type Y or n, aborting!"; break;;
 esac;
